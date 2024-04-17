692e3506b7b7f02fb6f63c45592951a9db73c5a3
Premalatha <81144129+premalatha1984@users.noreply.github.com>
2024-04-17 17:50:43 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/pawpointbook"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://sql8699655:3zeGWZ7Eun@sql8.freesqldatabase.com/sql8699655"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
